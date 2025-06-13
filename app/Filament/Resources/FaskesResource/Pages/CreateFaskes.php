93ba531f47aad20dc4a99e73353d164efe116dd3
muhamadr03 <mrrojali453@gmail.com>
2025-06-13 09:08:32 +0700

    perbaiki crud

--- app/Filament/Resources/FaskesResource/Pages/CreateFaskes.php
+++ app/Filament/Resources/FaskesResource/Pages/CreateFaskes.php
@@ -9,4 +9,11 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateFaskes extends CreateRecord
 {
     protected static string $resource = FaskesResource::class;
+    protected ?string $redirectUrl = null; // Default: null, akan mengarahkan ke halaman edit record yang baru dibuat
+    protected function getRedirectUrl(): string
+    {
+        // Mengarahkan kembali ke halaman list (daftar) setelah create
+        // Ini adalah perilaku yang paling umum diinginkan
+        return $this->getResource()::getUrl('index');
+    }
 }
--- app/Filament/Resources/ProvinsiResource/Pages/EditProvinsi.php
+++ app/Filament/Resources/ProvinsiResource/Pages/EditProvinsi.php
@@ -16,4 +16,11 @@ class EditProvinsi extends EditRecord
             Actions\DeleteAction::make(),
         ];
     }
+    protected ?string $redirectUrl = null; // Default: null, akan mengarahkan ke halaman edit record yang baru dibuat
+    protected function getRedirectUrl(): string
+    {
+        // Mengarahkan kembali ke halaman list (daftar) setelah create
+        // Ini adalah perilaku yang paling umum diinginkan
+        return $this->getResource()::getUrl('index');
+    }
 }
